477ba655b17d1d691d22944c2ed11e5f7d6d35d5
Winnerforlife <xahter.2000@gmail.com>
2020-09-18 18:36:12 +0300

    add gitignore

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
-import telebot
+# import telebot
+# import setings
 
-bot = telebot.TeleBot("1342433720:AAHz-6NJz4tWCF2_cdQdKlxjXZGI_xTRx6E", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
+# bot = telebot.TeleBot(TOKEN, parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.send_message(message.chat.id, "Эй, Дебил? Работает?")
+# @bot.message_handler(commands=['start', 'help'])
+# def send_welcome(message):
+# 	bot.send_message(message.chat.id, "Эй, Дебил? Работает?")
 
-if __name__ == "__main__":
-    bot.polling(none_stop=True)
+# if __name__ == "__main__":
+#     bot.polling(none_stop=True)
