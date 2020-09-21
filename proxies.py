f489433d26ca87c1ae2e74e79fd5ef7f9d76dfd6
Winnerforlife <xahter.2000@gmail.com>
2020-09-21 10:32:53 +0300

    proxy - works

--- proxies.py
+++ proxies.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as BS
 
-link = 'https://spys.one/proxys/RU/'
+link = 'http://foxtools.ru/Proxy?country=RU&al=True&am=True&ah=True&ahs=True&http=True&https=True'
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 Firefox/47.0',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
@@ -12,17 +12,15 @@ def ip_pars():
 
     if r.status_code == 200:
         soup = BS(r.content, "lxml")
-
-        # table = soup.find('tr', attrs={'class': 'spy1xx'})
-        x = soup.find('td', attrs={'colspan': '1'})
-        ip_info = x.find_all('font', attrs={'class': 'spy14'})
         
-        for ip in ip_info:
-            data = {
-                'ip': ip.text
+        for ip in soup.find_all('input', attrs={'class': 'ch'}):
+            
+            list_ip = {
+                'ip': ip.get('value')
             }
 
-            print(data)
+            print(list_ip)
+
     else:
         print("ОШИБКА, ДЕБИЛ!!!!!" + " " + r.status_code)
         
